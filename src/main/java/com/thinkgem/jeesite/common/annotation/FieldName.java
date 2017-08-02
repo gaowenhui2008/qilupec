dee9ec466e63c0038229d2b3a0c8eea73a8e2632
jikejikejike <2038076328@qq.com>
2017-08-02 18:11:00 +0800

    gaowenhui2070802commit

--- src/main/java/com/thinkgem/jeesite/common/annotation/FieldName.java
+++ src/main/java/com/thinkgem/jeesite/common/annotation/FieldName.java
@@ -6,12 +6,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * bean中文名注解
+ * bean中文名注解  
  */
 @Target(ElementType.METHOD)  
 @Retention(RetentionPolicy.RUNTIME)  
 public @interface FieldName {
 
-	String value();
+	String value();     
 	
 }
